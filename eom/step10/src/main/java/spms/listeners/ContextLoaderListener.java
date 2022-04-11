3312f1495ece342a2cb470b8d56d6f22bd75b425
Changu Kim <cgkim449@gmail.com>
2022-04-11 23:11:43 +0900

    refactor: 웹 앱에서 관리하는 DataSource 적용

---
+++ eom/step10/src/main/java/spms/listeners/ContextLoaderListener.java
@@ -0,0 +1,44 @@
+package spms.listeners;
+
+import java.sql.SQLException;
+
+// DBConnectionPool Àû¿ë
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+
+import spms.dao.MemberDao;
+
+@WebListener
+public class ContextLoaderListener implements ServletContextListener {
+  BasicDataSource ds;
+  
+  @Override
+  public void contextInitialized(ServletContextEvent event) {
+    try {
+      ServletContext sc = event.getServletContext();
+      
+      ds = new BasicDataSource();
+      ds.setDriverClassName(sc.getInitParameter("driver"));
+      ds.setUrl(sc.getInitParameter("url"));
+      ds.setUsername(sc.getInitParameter("username"));
+      ds.setPassword(sc.getInitParameter("password"));
+      
+      MemberDao memberDao = new MemberDao();
+      memberDao.setDataSource(ds);
+
+      sc.setAttribute("memberDao", memberDao);
+
+    } catch(Throwable e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override
+  public void contextDestroyed(ServletContextEvent event) {
+	  try { if (ds != null) ds.close(); } catch (SQLException e) {}
+  }
+}
