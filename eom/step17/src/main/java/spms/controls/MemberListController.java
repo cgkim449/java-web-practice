4d94fc864dd653fed9e26c50936de2b73f1d3f9c
Changu Kim <cgkim449@gmail.com>
2022-04-13 01:30:39 +0900

    refactor: 애노테이션을 이용한 의존 객체 관리

---
+++ eom/step17/src/main/java/spms/controls/MemberListController.java
@@ -0,0 +1,23 @@
+package spms.controls;
+
+import java.util.Map;
+
+import spms.annotation.Component;
+import spms.dao.MySqlMemberDao;
+
+@Component("/member/list.do")
+public class MemberListController implements Controller{
+	MySqlMemberDao memberDao;
+	
+	public MemberListController setMemberDao(MySqlMemberDao memberDao) {
+		this.memberDao = memberDao;
+		return this;
+	}
+
+	@Override
+	public String execute(Map<String, Object> model) throws Exception {
+		// MemberDao memberDao = (MemberDao)model.get("memberDao");
+		model.put("members", memberDao.selectList());
+		return "/member/MemberList.jsp";
+	}
+}
